e82a9f424d06e3ec51d98197ecf2650e17d36ea2
Landon Cayia <lcayia@uvm.edu>
2021-02-20 13:35:08 -0500

    Moved comments outside of methods

--- Parser.java
+++ Parser.java
@@ -5,32 +5,30 @@ import java.util.regex.Pattern;
 public class Parser {
     private int number;
 
+    // Eadoin and Zach need to be involved with this, but we can create a dummy table in the meantime.
     public static boolean loadTables() {
-        // Eadoin and Zach need to be involved with this, but we can create a dummy table in the meantime.
-
         // This method connects to the database and populates the tables
         return false;
     }
 
+    // Eadoin and Zach
     public static boolean populateTable(String table, String csv) {
-        // Eadoin and Zach
-
         // This method adds the data from the given csv into the table, returns boolean for
         // whether it was successful or not.
         return false;
     }
 
+    // All of us
     public static String findData(String group1, String group2, String group3) {
-        // All of us
+        
 
         // Takes the parameters from the regEx and inputs them into an SQL query. Returns
         // the information returned by the SQL statement
         return "a";
     }
 
+    // Landon and Isabelle can mess around with this maybe -- stretch goal
     public static ArrayList findMultiple(String table, String param, String operation, double value) {
-        // Landon and Isabelle can mess around with this maybe -- stretch goal
-
         // Stretch goal function, takes in which table is being searched, the column being searched,
         // the operation ( < / > / = ), and the value. This works like findData, but it uses an
         // ArrayList to return multiple data points from the table that fit the description.
@@ -38,30 +36,26 @@ public class Parser {
         return placeHolder;
     }
 
+    // Isabelle
     public static boolean validateInput(String input) {
-        // Isabelle
-
         // checks that the user has only entered valid commands and prints an error
         // message otherwise
         return false;
     }
 
+    // Isabelle
     public static void printHelp() {
-        // Isabelle
-
         //prints the help menu
         System.out.println("This is help");
     }
 
+    // Landon
     public static void printData(String data) {
-        // Landon
-
         // uses output from findData to print the requested data
     }
 
+    // Landon
     public static String[] parseCommand(String input) {
-        // Landon
-
         // uses if statements and regex to determine what data the user is requesting,
         // then calls findData and printData to output the result
         String[] placeHolder = new String[3];
